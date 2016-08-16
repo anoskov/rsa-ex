2419e42e1ad4edf6be2a00cacf437fcc588c1288
Andrey Noskov <anoskov@ccsteam.ru>
2016-08-16 09:16:05 +0300

    add generate_private_key

--- lib/rsa_ex.ex
+++ lib/rsa_ex.ex
@@ -1,2 +1,14 @@
 defmodule RsaEx do
+  @doc """
+  Generates RSA 2048 private key
+  """
+  @spec generate_private_key :: {atom, String.t}
+  def generate_private_key do
+    private_key = "ExPrivateKey.pem"
+    {_, 0} = System.cmd "openssl", [ "genrsa", "-out", private_key, "2048"], [stderr_to_stdout: true]
+    {:ok, priv} = File.read(private_key)
+    {_, 0} = System.cmd "rm", ["-f", private_key]
+
+    {:ok, priv}
+  end
 end
