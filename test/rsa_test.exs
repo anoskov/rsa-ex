8e00cb091a2ac77e66e26db0bcb5201db6d3ccab
Andrey Noskov <anoskov@ccsteam.ru>
2016-08-16 10:28:20 +0300

    add tests

---
+++ test/rsa_test.exs
@@ -0,0 +1,49 @@
+defmodule RsaEx.RSATest do
+  use ExUnit.Case, async: true
+
+  test "generate_private_key() generates string" do
+    {:ok, priv} = RsaEx.generate_private_key
+
+    assert is_binary(priv)
+  end
+
+  test "generate_public_key(private) generates string" do
+    {:ok, priv} = RsaEx.generate_private_key
+    {:ok, pub} = RsaEx.generate_public_key(priv)
+
+    assert is_binary(pub)
+  end
+
+  test "generate_keypair() generates two string" do
+    {:ok, {priv, pub}} = RsaEx.generate_keypair
+
+    assert is_binary(priv)
+    assert is_binary(pub)
+  end
+
+  test "sign(msg, priv_key) generates binary" do
+    {:ok, priv} = RsaEx.generate_private_key
+    {:ok, signature} = RsaEx.sign("message", priv)
+
+    assert is_binary(signature)
+  end
+
+  test "verify(msg, signature, pub_key) generates true if valid" do
+    {:ok, priv} = RsaEx.generate_private_key
+    {:ok, pub} = RsaEx.generate_public_key(priv)
+    {:ok, signature} = RsaEx.sign("message", priv)
+    {:ok, valid} = RsaEx.verify("message", signature, pub)
+
+    assert valid
+  end
+
+  test "verify(msg, signature, pub_key) generates false if invalid" do
+    {:ok, priv} = RsaEx.generate_private_key
+    {:ok, pub} = RsaEx.generate_public_key(priv)
+    {:ok, signature} = RsaEx.sign("message", priv)
+    {:ok, invalid} = RsaEx.verify("messages", signature, pub)
+
+    assert invalid == false
+  end
+
+end
