83f5c536b27ebbcb652bfb155756f6fc8a05a9cb
Nicholas Wieland <ngw@nofeed.org>
2016-12-12 11:57:58 +0000

    Let's create keys of different sizes

--- lib/rsa_ex.ex
+++ lib/rsa_ex.ex
@@ -5,13 +5,13 @@ defmodule RsaEx do
   @type public_key :: String.t
 
   @doc """
-  Generates RSA 2048 private key
+  Generates RSA private key
       iex> {:ok, priv} = RsaEx.generate_private_key
   """
   @spec generate_private_key :: {atom, private_key}
-  def generate_private_key do
+  def generate_private_key(bits \\ "2048") do
     private_key = "ExPrivateKey.pem"
-    {_, 0} = System.cmd "openssl", [ "genrsa", "-out", private_key, "2048"], [stderr_to_stdout: true]
+    {_, 0} = System.cmd "openssl", [ "genrsa", "-out", private_key, bits], [stderr_to_stdout: true]
     {:ok, priv} = File.read(private_key)
     File.rm!(private_key)
 
@@ -19,7 +19,7 @@ defmodule RsaEx do
   end
 
   @doc """
-  Generates RSA 2048 public key
+  Generates RSA public key
       iex> {:ok, priv} = RsaEx.generate_private_key
       iex> {:ok, pub} = RsaEx.generate_public_key(priv)
   """
@@ -39,15 +39,15 @@ defmodule RsaEx do
   end
 
   @doc """
-  Generates RSA 2048 private/public keypair
+  Generates RSA private/public keypair
       iex> {:ok, {priv, pub}} = RsaEx.generate_keypair
   """
   @spec generate_keypair :: {atom, {private_key, public_key}}
-  def generate_keypair do
+  def generate_keypair(bits \\ "2048") do
     private_key_name = "ExPrivateKey.pem"
     public_key_name = "ExPublicKey.pem"
 
-    {_, 0} = System.cmd "openssl", [ "genrsa", "-out", private_key_name, "2048"], [stderr_to_stdout: true]
+    {_, 0} = System.cmd "openssl", [ "genrsa", "-out", private_key_name, bits], [stderr_to_stdout: true]
     {_, 0} = System.cmd "openssl",
       [ "rsa", "-pubout", "-in" , private_key_name, "-out", public_key_name ], [stderr_to_stdout: true]
     {:ok, priv} = File.read(private_key_name)
